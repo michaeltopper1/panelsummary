3935daaa350377bc15b76ee59222c6789dffe470
Michael Topper <miketopper123@gmail.com>
2022-09-09 13:57:55 -0700

    feature: add failure if over 5 panels

--- R/add_panels.R
+++ R/add_panels.R
@@ -26,6 +26,8 @@ add_panels <- function(output, num_panels, bold = F, italic = T, latex_gap_space
       kableExtra::pack_rows(paste("Panel C:", panel_labels[3]), rows_per_model[2] + 1, rows_per_model[3], bold = bold, italic = italic, latex_gap_space = "0.5cm", hline_after = hline_after) |>
       kableExtra::pack_rows(paste("Panel D:", panel_labels[4]), rows_per_model[3] + 1, rows_per_model[4], bold = bold, italic = italic, latex_gap_space = "0.5cm", hline_after = hline_after) |>
       kableExtra::pack_rows(paste("Panel E:", panel_labels[5]), rows_per_model[5] + 1, rows_per_model[5], bold = bold, italic = italic, latex_gap_space = "0.5cm", hline_after = hline_after)
+  } else if (num_panels > 5){
+    stop("panelsummary does not support models with over 5 panels to incentivize the author to maintain conciseness.")
   }
 }
 
@@ -64,5 +66,7 @@ add_panels_cfe <- function(output, num_panels, bold = F, italic = T, latex_gap_s
       kableExtra::pack_rows(paste("Panel E:", panel_labels[5]), rows_per_model[5] + 1, rows_per_model[5], bold = bold, italic = italic, latex_gap_space = "0.5cm", hline_after = hline_after) |>
       kableExtra::row_spec(rows_per_model[num_panels + 1] - (rows_per_model[num_panels + 1] - rows_per_model[num_panels]), hline_after=hline_after_fe) |>
       kableExtra::row_spec(rows_per_model[num_panels + 1], hline_after = T)
+  } else if (num_panels > 5){
+    stop("panelsummary does not support models with over 5 panels to incentivize the author to maintain conciseness.")
   }
 }
